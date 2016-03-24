896ca5c2155fa6457a84e6fdd8a9e5b0fa9c2530
landlessness <brian@landlessness.net>
2016-03-24 18:59:38 -0400

    dropped location for parent driver. bumped version.

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var Device = require('zetta-device');
 var util = require('util');
 
-var HoneywellTotalConnect = module.exports = function(soap, locationID, device) {
+var HoneywellTotalConnect = module.exports = function(soap, device) {
   Device.call(this);
   this._soap = soap;
-  this.LocationID = locationID;
-  this._setProperties(device);
-};
-util.inherits(HoneywellTotalConnect, Device);
-
-HoneywellTotalConnect.prototype._setProperties = function(device) {
-  this.id = this.DeviceID = device.DeviceID;
+  this.DeviceID = device.DeviceID;
   this.DeviceName = device.DeviceName;
   this.DeviceSerialNumber = device.DeviceSerialNumber;
 
@@ -19,7 +13,9 @@ HoneywellTotalConnect.prototype._setProperties = function(device) {
     var flagKeyValue = flags[i].split('=');
     this[flagKeyValue[0]] = flagKeyValue[1];
   }
-}
+  
+};
+util.inherits(HoneywellTotalConnect, Device);
 
 HoneywellTotalConnect.prototype._isValidSession = function(sessionID) {
   return sessionID === this._soap._sessionID;
